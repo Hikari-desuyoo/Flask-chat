1fc9839f17da6ba9a4c0d96b4c58138b7b567b86
hikaridesuyoo <andre123luiz.al@gmail.com>
2021-04-16 22:24:23 -0300

    mainly esthetic improvements and cleared db

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///chat_history.db'
 app.config["SECRET_KEY"] = 'secret'
 
+#setting database for chat history
+chat_history = SQLAlchemy(app)
+
+class Message(chat_history.Model):
+    content = chat_history.Column(chat_history.String(msg_char_limit))
+    user = chat_history.Column(chat_history.String(20))
+    date = chat_history.Column(chat_history.DateTime, default=datetime.utcnow, primary_key=True)
+
+
 #socketio
 socketio = SocketIO(app)
 
@@ -21,7 +30,7 @@ def handle_message(msg_list):
     user_info={}
     user_info['user'] = user = msg_list[0]
     user_info['message'] = msg_input = msg_list[1]
-    invalid_cases = [len(msg_input)>msg_char_limit, len(msg_input)==0, len(user)==0]
+    invalid_cases = [len(msg_input)>msg_char_limit, len(msg_input)==0, type(user)==str and len(user)==0]
     invalid_text = [f"Sua mensagem excede o número máximo de caracteres([msg_char_limit])",
                     "Você não pode enviar uma mensagem em branco",
                     "Você não pode usar um nome de usuário em branco"]
@@ -36,20 +45,24 @@ def handle_message(msg_list):
     if not invalid:
         #accepts message on database
         new_msg = Message(content=msg_input, user=user)
+        user_info["date"] = datetime.utcnow().strftime("%H:%M")
         chat_history.session.add(new_msg)
         chat_history.session.commit()
         
         emit("clear", "")#clear input field if valid msg
-        emit("message", msg_list, broadcast=True)
+        emit("message", user_info, broadcast=True)
 
-#setting database for chat history
-chat_history = SQLAlchemy(app)
+@socketio.on('system_msg')
+def handle_system_msg(msg):
+    print("from system:", msg)
+    new_msg = Message(content=msg, user="")
+    chat_history.session.add(new_msg)
+    chat_history.session.commit()
+    msg = f"({datetime.utcnow().strftime('%H:%M')}){msg}"
+    emit("system", msg, broadcast=True)
 
-class Message(chat_history.Model):
-    content = chat_history.Column(chat_history.String(msg_char_limit))
-    user = chat_history.Column(chat_history.String(20))
-    date = chat_history.Column(chat_history.DateTime, default=datetime.utcnow, primary_key=True)
-            
+
+#routing
 @app.route('/', methods=["POST", "GET"])
 def chat_page():
     #starts html with chat history
@@ -71,6 +84,5 @@ def dated_url_for(endpoint, **values):
                                  endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
-
 
 socketio.run(app, debug = True)
---
+++ static/js/chat.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    //define all messages classes accordingly
+    var user = $("#user_input").val();
+    var all_msg = $("#chatbox > ."+user+"_msg");
+  
+    for(var i=0; i<all_msg.length; i++){
+        var msg_div = all_msg[i];
+        msg_div.classList.add("user")
+        
+    } 
+
+    //deals with socket
+    var socket = io.connect("http://127.0.0.1:5000/");
+
+    socket.on("connect", function(){
+        console.log("Conectado")
+        socket.emit("system_msg", "Um usuário se conectou")
+    });
+
+    function display_msg(msg){
+        $("#chatbox").append(msg);
+        scroll();
+    }
+
+    function scroll(){
+        var chatbox = document.getElementById("chatbox")
+        chatbox.style.height = (window.screen.height-parseInt(window.screen.height/2.6)).toString()+'px';
+        chatbox.scrollTop = chatbox.scrollHeight - chatbox.clientHeight;
+    }
+
+    socket.on("message", function(msg_dict){
+        console.log(msg_dict);
+        user = $("#user_input").val()
+        if(msg_dict["user"]==user){
+            var msg = "<div class=user>("
+        }else{
+            var msg = "<div class=other_user>("
+        }
+        msg += msg_dict["date"]+")["+msg_dict["user"]+"]:"+msg_dict["message"]+"</div>";
+        display_msg(msg);
+    });
+
+    socket.on("system", function(msg){
+        console.log(msg);
+        var msg = "<div class=system>~"+msg+"~</div>";
+        display_msg(msg);
+    });
+
+    socket.on("clear", function(unimportant){//clear msg input
+        $("#msg_input").val("")
+    })
+
+    function send_msg(){
+        socket.emit("message", [$("#user_input").val(), $("#msg_input").val()])
+    }
+
+    $("#send_input").click(send_msg)//send msg
+    document.getElementById("msg_input").addEventListener("keydown", function(event){//enter also sends msg
+        if(event.keyCode === 13){
+            send_msg()
+        }
+    })
+
+
+});
